e74c7c47b528b66f13f0216614db59f7753952b8
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-11-14 08:50:56 +0800

    Create purchase_items table for Purchase.Item

---
+++ priv/repo/migrations/20171114004625_create_items.exs
@@ -0,0 +1,13 @@
+defmodule Cwms.Repo.Migrations.CreateItems do
+  use Ecto.Migration
+
+  def change do
+    create table(:purchase_items) do
+      add :model_id, :string
+      add :quantity_requested, :integer
+      add :quantity_units, :string
+      add :unit_price_cents, :integer
+      add :description, :string
+    end
+  end
+end
